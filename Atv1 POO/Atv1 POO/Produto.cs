640af4a9e5689b9d19a8efa05e7f481456e97ed4
Samuel Felipe <102596618+samzik4@users.noreply.github.com>
2024-03-20 22:15:12 -0400

    Add files via upload

---
+++ Atv1 POO/Atv1 POO/Produto.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Convert;
+
+namespace Atv1_POO
+{
+    public class Produto
+    {
+        //Código do produto, descrição (o nome do produto), unidade (LT, CX, KG, UN, MT), preço, quantidade em estoque.
+        private string id;
+        private string descricao;
+        private string unidade;
+        private double preco;
+        private double quantidade;
+
+       public string getId() 
+        {
+            return id;
+        }
+
+        public string getDescricao()
+        {
+            return descricao;
+        }
+        public string getUnidade() 
+        {
+            return unidade;
+        }
+        public double getPreco()
+        {
+            return preco;
+        }
+        public double getQuantidade()
+        {
+            return quantidade;
+        }
+
+
+        public void setId(string id)
+        {
+            this.id = id;
+        }
+        public void setDescricao(string descricao)
+        {
+            this.descricao = descricao;
+        }
+
+        public void setUnidade(string unidade)
+        {
+            if(unidade == "L" || unidade == "CX" || unidade == "KG" || unidade == "UN" || unidade == "MT" || unidade == "l" || unidade == "cx" || unidade == "kg" || unidade == "un" || unidade == "mt")
+            {
+                this.unidade = unidade;
+            }
+            else
+            {
+                Console.WriteLine("essa unidade não ta certa não");
+            }
+        }
+
+        public void setPreco(double preco)
+        {   
+            if(preco > 0)
+            {
+                this.preco = preco;
+
+            }
+            else
+            {
+                Console.WriteLine("a gente não faz caridade!! quero meus lucros $$$$");
+            }
+                
+        }
+
+        public void setQuantidade(double quantidade)
+        {
+            this.quantidade = quantidade;
+        }
+    }
+}
---
+++ Atv1 POO/Atv1 POO/Program.cs
@@ -0,0 +1,83 @@
+﻿using Atv1_POO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Convert;
+using static System.Console;
+
+public class program
+{
+    static void Main(string[] args)
+    {
+        List<Produto> listaProdutos = new List<Produto>();
+        int op = 0;
+        do
+        {
+            Clear();
+            WriteLine("mozao, vc quer oq?");
+            WriteLine("1 - cadastrar produto");
+            WriteLine("2 - consultar todos os produtos");
+            WriteLine("3 - apagar algum produto da lista");
+            WriteLine("4 - sair");
+            op = ToInt32(ReadLine());
+            switch (op)
+            {
+                case 1:
+                    do
+                        {
+                            Produto prod = new Produto();
+                            WriteLine("insire o codigo de barras");
+                            prod.setDescricao(ReadLine());
+                            WriteLine("insire a descrição");
+                            prod.setDescricao(ReadLine());
+                            WriteLine("insira a unidade");
+                            prod.setUnidade(ReadLine());
+                            WriteLine("insira o preço");
+                            prod.setPreco(ToDouble(ReadLine()));
+                            WriteLine("insira a quantidade");
+                            prod.setQuantidade(ToDouble(ReadLine()));
+                            listaProdutos.Add(prod);
+
+                            Clear();
+                            WriteLine("repeteco mor?");
+                            WriteLine("1 - simmm");
+                            WriteLine("2 - nai");
+                            op = ToInt32(ReadLine());
+                        }
+                    while (op == 1);
+                    break;
+
+                case 2:
+                    foreach(Produto prodi in listaProdutos)
+                    {
+                        WriteLine("----------------------------------");
+                        WriteLine("Descrição: "+ prodi.getDescricao());
+                        WriteLine("Unidade: "+ prodi.getUnidade());
+                        WriteLine("Preço: "+ prodi.getPreco());
+                        WriteLine("Quantidade: "+ prodi.getQuantidade());
+                        WriteLine("----------------------------------");
+                    }
+                    Console.ReadKey();
+
+
+                    break;
+
+                case 3:
+                    WriteLine("digite o codigo de barras do produto que sera apagado");
+                    string cod = ReadLine();
+                    Produto pExcluir = listaProdutos.FirstOrDefault(x => x.getId() == cod);
+                    listaProdutos.Remove(pExcluir);
+                    break;
+
+                default:
+
+                    break;
+            }
+
+        }
+        while (op != 3);
+
+    }
+}
